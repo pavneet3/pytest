2e05ca21c7d74ee32ecb5071e1f31fb1a2833ddb
Akshay <agarwal3akshay@gmail.com>
2019-08-29 21:59:00 +0530

    Reversal of a number

--- Week1/reversenumber.py
+++ Week1/reversenumber.py
@@ -2,3 +2,12 @@
 
 # take a input from user as number and print reverse of no.
 
+num = int(input("Enter a number: "))
+rev = 0
+while(num > 0):
+    rem = num % 10
+    rev = (rev * 10) + rem
+    num = num // 10
+
+print("\n Reverse of entered number is = %d" %rev)
+
