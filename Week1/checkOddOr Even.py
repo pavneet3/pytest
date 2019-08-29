2a4de1f0d63a593a815e0639216f190957f3b248
Akshay <agarwal3akshay@gmail.com>
2019-08-29 13:57:10 +0530

    To check whether Even or Odd

--- Week1/checkOddOr Even.py
+++ Week1/checkOddOr Even.py
@@ -1,3 +1,9 @@
 # conditional check
 
 # take input from user and print if the number is odd or even
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("number is Even")
+else:
+   print("number is Odd")
