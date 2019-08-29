ebc0fa7b8381a538a5638c16c5266a0f0ccfeec8
Akshay <agarwal3akshay@gmail.com>
2019-08-29 22:05:51 +0530

    Sum of two numbers

--- Week1/printsumoftwonumbers.py
+++ Week1/printsumoftwonumbers.py
@@ -1,3 +1,8 @@
 #simple arithmatics
 
 #take two variables and take input from user and return sum of those two no.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("The sum is: %d" %sum)
