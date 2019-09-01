31129c3b0085e41cb7a8477d35b215f1cf006471
Akshay <agarwal3akshay@gmail.com>
2019-09-01 15:21:59 +0530

    Check vowel or not

--- Week1/checkvovels.py
+++ Week1/checkvovels.py
@@ -1,3 +1,10 @@
 # adv conditional check
 
 # take input as only one character and print if the character is vovel or not.
+
+ch = input("Enter a character: ")
+
+if( ch == 'a' or ch == 'A' or ch == 'e' or ch == 'E' or ch == 'i' or ch == 'I' or ch == 'o' or ch == 'O' or ch == 'u' or ch == 'U'):
+    print("It is a vowel")
+else :
+    print("It is a consonant")
