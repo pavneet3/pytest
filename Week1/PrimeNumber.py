919e8ddc6c45f23b0537f5ad4a8be7cc99d4df17
Akshay <agarwal3akshay@gmail.com>
2019-09-02 15:08:47 +0530

    There is a small error

--- Week1/PrimeNumber.py
+++ Week1/PrimeNumber.py
@@ -1,1 +1,10 @@
 # input a number and print if a number is prime or not.
+
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2,num):
+        if(num % i == 0):
+            print(num," is not a prime number")
+            break
+        else:
+            print(num, "is a prime number")
