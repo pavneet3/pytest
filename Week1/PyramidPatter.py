4800a0d77e2d1ff56e74b7431b12381242518c9f
Akshay <agarwal3akshay@gmail.com>
2019-09-03 23:42:42 +0530

    Pyramid Pattern

--- Week1/PyramidPatter.py
+++ Week1/PyramidPatter.py
@@ -6,3 +6,10 @@
 #            ****
 #            *****
 #            ******   
+
+n = int(input("Enter the height of pyramid: "))
+a = 0
+while a < n:
+        print("*"*a)
+        a = a+1
+        
