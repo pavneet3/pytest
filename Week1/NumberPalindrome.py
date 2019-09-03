041b838a3735eab50e5ba5295a111c07769e7aa4
Akshay <agarwal3akshay@gmail.com>
2019-09-03 09:40:12 +0530

    To check whether a number is palindrome or not

--- Week1/NumberPalindrome.py
+++ Week1/NumberPalindrome.py
@@ -2,3 +2,9 @@
 
 # Palindrome number is -> 123321 -> reverse number is same as the original
 # 123321 reverse of this number is 123321
+
+num = int(input("Enter a number to check whether palindrome or not"))
+
+res = str(num) == str(num)[::-1]
+
+print("Is the number palindrome ? : " + str(res))
