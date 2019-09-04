5056ed8dcc3d4735f1c2b38829917e9b49572b34
Akshay <agarwal3akshay@gmail.com>
2019-09-04 13:12:07 +0530

    Pyramid Pattern 2

--- Week1/PyramidPattern2.py
+++ Week1/PyramidPattern2.py
@@ -6,3 +6,17 @@
 #                     *******
 #                    *********
 #                   ***********   
+
+n = int(input("Enter the height of tree: "))
+
+a = 1
+e = 1
+
+while a < n:
+    for c in range (0,n-a):
+        print(" ",end=" ")
+    for d in range (0,e):
+        print("*",end=" ")
+    print()
+    e = e+2
+    a = a+1
